11987a55d41461e17bbd1fc3a66137bfa7ba364d
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-06-08 08:08:41 -0500

    Added Gemfile

---
+++ lib/hydra-access-controls.rb
@@ -0,0 +1,9 @@
+require "hydra-access-controls/version"
+
+module Hydra
+  module Access
+    module Controls
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/hydra-access-controls/version.rb
@@ -0,0 +1,7 @@
+module Hydra
+  module Access
+    module Controls
+      VERSION = "0.0.1"
+    end
+  end
+end
