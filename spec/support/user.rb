189434065c7e23177b4bff3ed6f4d3240acd429b
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2012-07-05 15:26:28 -0500

    spec/support/user

---
+++ spec/support/user.rb
@@ -0,0 +1,25 @@
+class User
+  attr_accessor :uid, :email, :password, :affiliations, :new_record
+  
+  def user_key
+    uid
+  end
+  
+  def new_record?
+    new_record == true
+  end
+  
+  def is_being_superuser?(session)
+    # do nothing -- stubbing deprecated behavior
+  end
+  
+  def save
+    # do nothing!
+  end
+  
+  def save!
+    save
+    return self
+  end
+  
+end
