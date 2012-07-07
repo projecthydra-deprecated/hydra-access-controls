475c32ad75363cfafcacf4ba8a818cf8f717b74f
Chris Beer <cabeer@stanford.edu>
2012-07-06 18:01:49 -0700

    fix path to solr.yml

--- spec/support/blacklight.rb
+++ spec/support/blacklight.rb
@@ -2,6 +2,6 @@ require 'blacklight'
 
 module Blacklight
   def self.solr_file
-    File.expand_path(File.join(File.dirname(__FILE__), "..", "config", "solr.yml"))
+    File.expand_path(File.join(File.dirname(__FILE__), "config", "solr.yml"))
   end
 end
